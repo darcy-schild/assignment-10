8814c76bab1a62cc01ef4131cc295ed3462f4936
Darcy Schild <darcyschild@gmail.com>
2018-11-09 15:59:17 -0500

    first commit

---
+++ scripts/darcyjs.js
@@ -0,0 +1,34 @@
+
+$(document).ready(() => {
+
+
+    $('.card').hide();
+
+    $('#button1').on('click', () => {
+      $('.card').hide();
+       $('#gators').show();
+
+  });
+
+    $('#button2').on('click', () => {
+      $('.card').hide();
+        $('#laptop').show();
+
+  });
+
+    $('#button3').on('click', () => {
+        $('.card').hide();
+          $('#glasses').show();
+
+});
+
+  $('#button4').on('click', () => {
+      $('.card').hide();
+        $('#book').show();
+
+});
+
+
+
+
+});
